a571f97a9a0d62d4534c5f26176f4e6b58046e2c
Pedro P. Candel <kusorbox@gmail.com>
2012-04-12 13:21:56 +0200

    Added methods to CRUD workflows

--- test/moray-backend.test.js
+++ test/moray-backend.test.js
@@ -36,6 +36,93 @@ test('setup', function (t) {
 });
 
 
+test('add a workflow', function (t) {
+  factory.workflow({
+    name: 'A workflow',
+    chain: [ {
+      name: 'A Task',
+      timeout: 30,
+      retry: 3,
+      body: function (job, cb) {
+        return cb(null);
+      }
+    }],
+    timeout: 180,
+    onError: [ {
+      name: 'Fallback task',
+      body: function (job, cb) {
+        return cb('Workflow error');
+      }
+    }]
+  }, function (err, workflow) {
+    t.ifError(err, 'add workflow error');
+    t.ok(workflow, 'add workflow ok');
+    aWorkflow = workflow;
+    t.ok(workflow.chain[0].uuid, 'add workflow chain task');
+    t.ok(workflow.onerror[0].uuid, 'add workflow onerror task');
+    t.end();
+  });
+});
+
+
+test('workflow name must be unique', function (t) {
+  factory.workflow({
+    name: 'A workflow',
+    chain: [ {
+      name: 'A Task',
+      timeout: 30,
+      retry: 3,
+      body: function (job, cb) {
+        return cb(null);
+      }
+    }],
+    timeout: 180,
+    onError: [ {
+      name: 'Fallback task',
+      body: function (job, cb) {
+        return cb('Workflow error');
+      }
+    }]
+  }, function (err, workflow) {
+    t.ok(err, 'duplicated workflow name err');
+    t.end();
+  });
+});
+
+
+test('get workflow', function (t) {
+  backend.getWorkflow(aWorkflow.uuid, function (err, workflow) {
+    t.ifError(err, 'get workflow error');
+    t.ok(workflow, 'get workflow ok');
+    t.equivalent(workflow, aWorkflow);
+    backend.getWorkflow(uuid(), function (err, workflow) {
+      t.equal(typeof (err), 'object');
+      t.equal(err.name, 'BackendResourceNotFoundError');
+      t.ok(err.message.match(/uuid/gi), 'unexisting workflow error');
+      t.end();
+    });
+  });
+});
+
+
+test('update workflow', function (t) {
+  aWorkflow.chain.push({
+    name: 'Another task',
+    body: function (job, cb) {
+      return cb(null);
+    }.toString()
+  });
+  aWorkflow.name = 'A workflow name';
+  backend.updateWorkflow(aWorkflow, function (err, workflow) {
+    t.ifError(err, 'update workflow error');
+    t.ok(workflow, 'update workflow ok');
+    t.ok(workflow.chain[1].name, 'Updated task ok');
+    t.ok(workflow.chain[1].body, 'Updated task body ok');
+    t.end();
+  });
+});
+
+
 test('teardown', function (t) {
   async.forEach(['wf_workflows', 'wf_jobs', 'wf_runners'],
     function (bucket, cb) {
