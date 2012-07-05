c554818a146716922055f77eab2cebd51ca9ca69
Pedro P. Candel <kusorbox@gmail.com>
2012-07-05 18:55:13 +0200

    Fixed path|fs.existsSync.

--- test/helper.js
+++ test/helper.js
@@ -1,18 +1,18 @@
 // Copyright 2012 Pedro P. Candel <kusorbox@gmail.com>. All rights reserved.
 
 var path = require('path'),
-    fs = require('fs');
-
+    fs = require('fs'),
+    existsSync = fs.existsSync || path.existsSync;
 var cfg = path.resolve(__dirname, './config.json'),
-    cfg_file = path.existsSync(cfg) ? cfg :
+    cfg_file = existsSync(cfg) ? cfg :
                path.resolve(__dirname, './config.json.sample'),
                config;
 
 module.exports = {
-  config: function () {
-    if (!config) {
-      config = JSON.parse(fs.readFileSync(cfg_file, 'utf-8'));
+    config: function () {
+        if (!config) {
+            config = JSON.parse(fs.readFileSync(cfg_file, 'utf-8'));
+        }
+        return config;
     }
-    return config;
-  }
 };
